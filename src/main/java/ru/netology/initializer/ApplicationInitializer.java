914a7e6f16e0db1086eb3e0f3b4c7e3155753ae5
Grigoriy Dyatlov <holy.crapicl@gmail.com>
2024-12-26 11:49:10 +0300

    add responseStatus to notFount ex add soft delete change PostRepositoryImpl

--- src/main/java/ru/netology/initializer/ApplicationInitializer.java
+++ src/main/java/ru/netology/initializer/ApplicationInitializer.java
@@ -1,6 +1,5 @@
 package ru.netology.initializer;
 
-
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
@@ -10,15 +9,15 @@ import javax.servlet.ServletContext;
 public class ApplicationInitializer implements WebApplicationInitializer {
     @Override
     public void onStartup(ServletContext servletContext) {
-        var context = new AnnotationConfigWebApplicationContext();
+        final var context = new AnnotationConfigWebApplicationContext();
         context.scan("ru.netology");
         context.refresh();
 
-        var dispatcherServlet = new DispatcherServlet(context);
-        var registration = servletContext.addServlet("app", dispatcherServlet);
+        final var servlet = new DispatcherServlet(context);
+        final var registration = servletContext.addServlet(
+                "app", servlet
+        );
         registration.setLoadOnStartup(1);
         registration.addMapping("/");
     }
 }
-
-
